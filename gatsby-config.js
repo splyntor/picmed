878aac1ee486dfdb23febd5bba3cd8c7568789c0
vvcb <vvcb.n1@gmail.com>
2019-09-08 00:38:00 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  pathPrefix: `/mini-gatsbyv2-material-kit-react`,
+  siteMetadata: {
+    title: 'Picadilly Medical',
+  },
+  plugins: [
+    'gatsby-plugin-resolve-src',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-material-ui',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options:
+      {
+        path: `./src/data/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
+  ],
+}
