8222aff5e2ad0cd49fcec4422d95f28b1a690003
vvcb <vvcb.n1@gmail.com>
2019-09-08 00:43:08 +0100

    More components

---
+++ bkup/Components/Components.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import { Link } from "gatsby";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import SectionBasics from "./Sections/SectionBasics.jsx.js";
+import SectionNavbars from "./Sections/SectionNavbars.jsx.js";
+import SectionTabs from "./Sections/SectionTabs.jsx.js";
+import SectionPills from "./Sections/SectionPills.jsx.js";
+import SectionNotifications from "./Sections/SectionNotifications.jsx.js";
+import SectionTypography from "./Sections/SectionTypography.jsx.js";
+import SectionJavascript from "./Sections/SectionJavascript.jsx.js";
+import SectionCarousel from "./Sections/SectionCarousel.jsx.js";
+import SectionCompletedExamples from "./Sections/SectionCompletedExamples.jsx.js";
+import SectionLogin from "./Sections/SectionLogin.jsx.js";
+import SectionExamples from "./Sections/SectionExamples.jsx.js";
+import SectionDownload from "./Sections/SectionDownload.jsx.js";
+
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+
+class Components extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          brand="Material Kit React"
+          rightLinks={<HeaderLinks />}
+          fixed
+          color="transparent"
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax image={require("assets/img/bg4.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+                <div className={classes.brand}>
+                  <h1 className={classes.title}>Material Kit React.</h1>
+                  <h3 className={classes.subtitle}>
+                    A Badass Material-UI Kit based on Material Design.
+                  </h3>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <SectionBasics />
+          <SectionNavbars />
+          <SectionTabs />
+          <SectionPills />
+          <SectionNotifications />
+          <SectionTypography />
+          <SectionJavascript />
+          <SectionCarousel />
+          <SectionCompletedExamples />
+          <SectionLogin />
+          <GridItem md={12} className={classes.textCenter}>
+            <Link to={"/login-page"} className={classes.link}>
+              <Button color="primary" size="lg" simple>
+                View Login Page
+              </Button>
+            </Link>
+          </GridItem>
+          <SectionExamples />
+          <SectionDownload />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withStyles(componentsStyle)(Components);
