ef6dff3e1ad409bc1f799dd92aa86b5c2a6ea6ea
vvcb <vvcb.n1@gmail.com>
2019-09-18 12:52:56 +0100

    More linting related

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/mini-gatsbyv2-material-kit-react`,
+  pathPrefix: '/mini-gatsbyv2-material-kit-react',
   siteMetadata: {
     title: 'Picadilly Medical',
   },
@@ -9,18 +9,17 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-sass',
     'gatsby-plugin-material-ui',
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-yaml`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-yaml',
     {
-      resolve: `gatsby-source-filesystem`,
-      options:
-      {
-        path: `./src/data/`
-      }
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/data/',
+      },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
@@ -31,8 +30,8 @@ module.exports = {
         icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
       },
     },
-    { 
-      resolve: `gatsby-plugin-purgecss`,
+    {
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
@@ -40,7 +39,7 @@ module.exports = {
         // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
+      },
     },
   ],
 }
